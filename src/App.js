2a6dd69448a324c0b7e0a2139d49348b00176897
Lázaro Alves <geo.eolo@gmail.com>
2021-06-17 15:50:20 -0300

    Adiciona layout a api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Layout from "./components/layout";
 
 function App() {
-  return <div className="App">github api</div>;
+  return <main>
+    <Layout>
+        <div>
+              <img source="https://avatars.githubusercontent.com/u/75143575?v=4" alt="Avatar of user"/>
+              <h1>Lázaro Alves</h1>
+              <h3>Username: </h3>
+              <span>Lázaro Alves</span>
+              <div>
+                  <div> 
+                    <h4>Followers</h4>
+                    <span>5</span>
+                  </div>
+                  <div> 
+                    <h4>Starreds</h4>
+                    <span>5</span>
+                  </div>
+                  <div> 
+                    <h4>Followings</h4>
+                    <span>5</span>
+                  </div>
+              </div>
+        </div>
+        <div>Repositories</div>
+        <div>Starreds</div>
+    </Layout>
+  </main>;
 }
 
 export default App;
