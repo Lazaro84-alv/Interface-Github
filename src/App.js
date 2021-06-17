d95f2d0820056173bc18ee6253b90675c3ad9b0c
Lázaro Alves <geo.eolo@gmail.com>
2021-06-17 18:14:58 -0300

    Adiciona style

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import Layout from "./components/layout";
+import Profile from './components/profile';
 
-function App() {
+const App = () => {
   return <main>
     <Layout>
-        <div>
-              <img source="https://avatars.githubusercontent.com/u/75143575?v=4" alt="Avatar of user"/>
-              <h1>Lázaro Alves</h1>
-              <h3>Username: </h3>
-              <span>Lázaro Alves</span>
-              <div>
-                  <div> 
-                    <h4>Followers</h4>
-                    <span>5</span>
-                  </div>
-                  <div> 
-                    <h4>Starreds</h4>
-                    <span>5</span>
-                  </div>
-                  <div> 
-                    <h4>Followings</h4>
-                    <span>5</span>
-                  </div>
-              </div>
-        </div>
+       <Profile />
         <div>Repositories</div>
         <div>Starreds</div>
     </Layout>
