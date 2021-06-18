95780cc647b2c49627ba5ac8078c71aa4243b432
Lázaro Alves <geo.eolo@gmail.com>
2021-06-18 14:05:29 -0300

    Adiciona layout header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Layout from "./components/layout";
 import Profile from './components/profile';
+import Repositories from "./components/repositories";
 import { ResetCSS } from './global/resetCSS';
+import GithubProvider from "./providers/github-provider";
 
 const App = () => {
   return <main>
-    <ResetCSS />
-    <Layout>
-       <Profile />
-        <div>Repositories</div>
-        <div>Starreds</div>
-    </Layout>
+    <GithubProvider>
+        <ResetCSS />
+        <Layout>
+          <Profile />
+          <Repositories />
+        </Layout>
+    </GithubProvider>
   </main>;
 }
 
--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as S from "./styled";
+import RepositoryItem from "../repository-item";
 
 const Repositories = () => {
   return <S.WrapperTabs
@@ -10,6 +11,18 @@ const Repositories = () => {
               <S.WrapperTab>Repositories</S.WrapperTab>
               <S.WrapperTab>Starred</S.WrapperTab>
             </S.WrapperTabList>
+            <S.WrapperTabPanel>
+              <RepositoryItem name="Acelera-o-Global-Dev-8-Avanade" 
+                linkToRepo="https://github.com/Lazaro84-alv/Acelera-o-Global-Dev-8-Avanade"
+                fullName="Lazaro84-alv/Acelera-o-Global-Dev-8-Avanade"
+              />
+            </S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+              <RepositoryItem name="API-NodeJs-gerenciando-contatinhos"
+                linkToRepo="https://github.com/Lazaro84-alv/API-NodeJs-gerenciando-contatinhos"
+                fullName="Lazaro84-alv/API-NodeJs-gerenciando-contatinhos"
+              />
+            </S.WrapperTabPanel>
 
         </S.WrapperTabs>;
 };
